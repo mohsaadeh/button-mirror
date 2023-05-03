218d348fb92a82ae4e0aeef9a8801687c43fb7c1
mohammad <moh.hsaadeh@gmail.com>
2023-05-03 15:54:10 +0300

    Create Button mirror

---
+++ camera.js
@@ -0,0 +1,10 @@
+let video = document.querySelector('#video');
+if(navigator.mediaDevices.getUserMedia){
+    navigator.mediaDevices.getUserMedia({video:true})
+    .then(function(stream){
+        video.srcObject = stream;
+    })
+    .catch(function(error){
+        console.log('Something went wrong');
+    })
+}
